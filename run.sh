da7f28a6d2b6540fb98dbb7a85ca6c24c4cd9d94
SiddhantSilwal <sidsil45@gmail.com>
2022-09-16 12:37:01 +0545

    Developer options modified

--- run.sh
+++ run.sh
@@ -107,6 +107,9 @@ elif [ $Choice=5 ]; then
     wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
+   #Slack
+    sudo apt install -y slack
+
 fi
 
 
